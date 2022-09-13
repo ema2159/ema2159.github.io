920875f3a19552dd3a4e55b4c73f8083a6cda171
Emmanuel Bustos <ema2159@gmail.com>
2022-09-13 19:56:02 +0300

    Mod: Externalize projects' data in their own file

---
+++ projectsData.js
@@ -0,0 +1,39 @@
+const projectData = [
+  {
+    name: "Centaur Tabs",
+    descItems: [
+      "Highly customizable tabs plugin for Emacs.",
+      "Over 500 GitHub stars and over 50.000 downloads.",
+    ],
+    github: "https://github.com/ema2159/centaur-tabs",
+  },
+  {
+    name: "Doom Themes",
+    descItems: [
+      "Doom Emacs's main theme package.",
+      "Over 1900 GitHub stars and 500.000 downloads.",
+    ],
+    github: "https://github.com/doomemacs/themes",
+  },
+  {
+    name: "Three.js robot arm",
+    descItems: [
+      "Interactive 3D robot arm with 6 degrees of freedom implemented using Three.js.",
+      "In the scene, a set of balls is randomly generated and placed on the floor. When a ball is clicked, the robot arm will pick the ball and place it in the trash bin.",
+      "Kinematics calculations were done using the Kinematics Javascript library. Animations were created using the Tween.js Javascript library."
+    ],
+    github: "https://github.com/ema2159/ThreejsPractice",
+    demo: "https://ema2159.github.io/ThreejsPractice/src/Exercises/Exercise5/index.html"
+  },
+  {
+    name: "Global warming visualization dashboard",
+    descItems: [
+      "Web dashboard with different visualizations of historical world temperature data, and world temperature anomaly.",
+      "Built using the D3.js framework."
+    ],
+    github: "https://github.com/ema2159/globalWarimingD3Visualization",
+    demo: "https://ema2159.github.io/globalWarimingD3Visualization/"
+  },
+];
+
+export default projectData;
